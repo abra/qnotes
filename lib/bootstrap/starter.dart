a3fb0a5c534327a01648f1d9e37c27862960c405
Aidar Khabibullin <aydar.kh@gmail.com>
2026-02-24 21:10:56 +0500

    Replace doc comment with regular

--- lib/bootstrap/starter.dart
+++ lib/bootstrap/starter.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
-/// Flutter initialization, error zone setup, runApp launch
+// Flutter initialization, error zone setup, runApp launch
 
 Future<void> starter() async {
   runApp(Placeholder());
